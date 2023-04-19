fa3dff1a7dbd5aa9c2247ea5e21242e474e67f0b
prayfortherain <prayfortherain@yandex.ru>
2023-04-19 21:08:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        String input = "1.32.123.345.322.434.66.43.43.21.112.122.1214544";
+        Pattern pattern = Pattern.compile("((\\d|\\d\\d|1\\d\\d|1\\d\\d|2[1;4]\\d|250)\\.){3}(\\d|1\\d\\d|1\\d\\d|2[1;4]\\d|250){1,3}");
+        Matcher matcher = pattern.matcher(input);
+        while(matcher.find())
+            System.out.println(matcher.group());
+    }
+}
