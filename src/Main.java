82b282e6e0d47f5a08ee1c2613546a13044705c6
prayfortherain <prayfortherain@yandex.ru>
2023-04-20 14:38:35 +0300

    change pattern to correct (finally)

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         //String input = "1.32.123.345.322.434.66.43.43.21.112.122.1214544.12.12.6544";
-        Pattern pattern = Pattern.compile("((\\d|\\d\\d|1\\d\\d|1\\d\\d|2[0;4]\\d|250)\\.){3}(250|2[0;4]\\d|1\\d\\d|\\d\\d|\\d)");
+        Pattern pattern = Pattern.compile("((25[0-5]|2[0-4]\\d|1\\d\\d|\\d\\d|\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d\\d|\\d\\d|\\d)");
         Matcher matcher = pattern.matcher(input);
         while(matcher.find())
             System.out.println(matcher.group());
