563e7add606cb40bcc213fe47bb99fcfe90d7a75
prayfortherain <prayfortherain@yandex.ru>
2023-04-20 14:04:53 +0300

    change pattern

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        //String input = "1.32.123.345.322.434.66.43.43.21.112.122.1214544";
-        Pattern pattern = Pattern.compile("((\\d|\\d\\d|1\\d\\d|1\\d\\d|2[1;4]\\d|250)\\.){3}(\\d|1\\d\\d|1\\d\\d|2[1;4]\\d|250){1,3}");
+        //String input = "1.32.123.345.322.434.66.43.43.21.112.122.1214544.12.12.6544";
+        Pattern pattern = Pattern.compile("((\\d|\\d\\d|1\\d\\d|1\\d\\d|2[0;4]\\d|250)\\.){3}(250|2[0;4]\\d|1\\d\\d|\\d\\d|\\d)");
         Matcher matcher = pattern.matcher(input);
         while(matcher.find())
             System.out.println(matcher.group());
